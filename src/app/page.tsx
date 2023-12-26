f797c8f01bf61ac61ddf73b58d4b2995b0e44373
chrosslaw <Chrosslaw@gmail.com>
2023-12-26 15:17:55 -0600

    style adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,9 @@ export default function Home() {
   if (!memes) fetchMemes();
   return (
     <div className="flex flex-col place-items-center">
-      <div className="flex flex-col md:flex-row place-items-center mb-4 gap-4">
+      <div className="flex flex-col md:flex-row place-content-center place-items-center w-full p-2 m-4 gap-4 shadow-inner-y shadow-gray-200  bg-gradient-to-r from-white from-20% via-slate-300 via-50% to-white to-80%">
         <button
-          className="border w-max bg-yellow-50 border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-2 text-xl"
+          className="border w-max bg-white border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-1 text-xl"
           onClick={() => {
             if (jokeType !== "Chuck Norris") {
               setJokeType("Chuck Norris");
@@ -61,7 +61,7 @@ export default function Home() {
           Chuck Norris Joke
         </button>
         <button
-          className="border w-max bg-yellow-50 border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-2  text-xl"
+          className="border w-max bg-white border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-1  text-xl"
           onClick={() => {
             if (jokeType !== "Tech") {
               setJokeType("Tech");
@@ -73,7 +73,7 @@ export default function Home() {
           Tech Joke
         </button>
         <button
-          className="border w-max bg-yellow-50 border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-2  text-xl"
+          className="border w-max bg-white border-black rounded-md hover:border-blue-400 shadow-md hover:shadow-xl p-1  text-xl"
           onClick={() => {
             setRandomNumber(Math.floor(Math.random() * 100));
             if (!showMeme) setShowMeme(true);
--- src/app/ui/meme.tsx
+++ src/app/ui/meme.tsx
@@ -1,25 +1,28 @@
 export default function Meme({ meme, showMeme, setShowMeme }) {
   const { name, url, height, width } = meme;
   return (
-    <div className="flex flex-col place-content-center m-8 p-4 border border-black rounded-lg shadow-lg">
-      <button
-        className="border bg-yellow-50 border-black rounded-full hover:border-gray-400 shadow-md hover:shadow-xl px-1 text-center self-end"
-        onClick={() => {
-          setShowMeme(false);
-        }}
-      >
-        x
-      </button>
-      <p className="m-2 p-2 text-center text-xl font-bold">{name}</p>
-      <a href="#" className="self-center">
-        <img
-          src={url}
-          alt={name}
-          height={height}
-          width={width}
-          className="m-4 p-4 "
-        />
-      </a>
+    <div className="flex flex-col place-content-center self-center m-8 p-4 w-full border-t-2">
+      <p className="text-center font-bold text text-2xl">A meme for you</p>
+      <div className="flex flex-col place-content-center self-center m-8 p-4 border border-black rounded-lg shadow-lg ">
+        <button
+          className="self-end border border-black rounded-full text-center align-baseline hover:border-gray-400 shadow-md hover:shadow-xl bg-black"
+          onClick={() => {
+            setShowMeme(false);
+          }}
+        >
+          <p className="rounded-full hover:shadow-xl px-1 pb-1 text-white">x</p>
+        </button>
+        <p className="m-2 p-2 text-center text-xl font-bold">{name}</p>
+        <a href="#" className="self-center">
+          <img
+            src={url}
+            alt={name}
+            height={height}
+            width={width}
+            className="m-4 p-4 "
+          />
+        </a>
+      </div>
     </div>
   );
 }
